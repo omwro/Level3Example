aa5366d583add3acb659994bb59f2a941980ae20
Omer Erdem <m.o.erdem@outlook.com>
2020-09-18 14:00:30 +0200

    #6  Swipe to remove reminder

--- app/src/main/java/nl/omererdem/madlevel3example/RemindersFragment.kt
+++ app/src/main/java/nl/omererdem/madlevel3example/RemindersFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.setFragmentResultListener
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_reminders.*
@@ -38,6 +39,8 @@ class RemindersFragment : Fragment() {
         rvReminders.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
         rvReminders.adapter = reminderAdapter
         rvReminders.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+
+        createItemTouchHelper().attachToRecyclerView(rvReminders)
     }
 
     private fun observeAddReminderResult() {
@@ -48,5 +51,24 @@ class RemindersFragment : Fragment() {
                 reminderAdapter.notifyDataSetChanged()
             } ?: Log.e("ReminderFragment", "Request triggered, but empty reminder text!")
         }
+    }
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                reminders.removeAt(position)
+                reminderAdapter.notifyDataSetChanged()
+            }
+        }
+        return ItemTouchHelper(callback)
     }
 }
