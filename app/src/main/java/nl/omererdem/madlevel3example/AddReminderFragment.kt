eb1f0588852dcd28a4f7588efdc679617c59b8fe
Omer Erdem <m.o.erdem@outlook.com>
2020-09-18 13:10:36 +0200

    #4.4 add-reminder logic added

--- app/src/main/java/nl/omererdem/madlevel3example/AddReminderFragment.kt
+++ app/src/main/java/nl/omererdem/madlevel3example/AddReminderFragment.kt
@@ -5,8 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_add_reminder.*
+
+const val REQ_REMINDER_KEY = "req_reminder"
+const val BUNDLE_REMINDER_KEY = "bundle_reminder"
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -23,5 +29,19 @@ class AddReminderFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        btnAddReminder.setOnClickListener {
+            onAddReminder()
+        }
+    }
+
+    private fun onAddReminder() {
+        val reminderText = etReminderName.text.toString()
+        if (reminderText.isNotBlank()) {
+            setFragmentResult(REQ_REMINDER_KEY, bundleOf(Pair(BUNDLE_REMINDER_KEY, reminderText)))
+            findNavController().popBackStack()
+        } else {
+            Toast.makeText(activity, R.string.not_valid_reminder, Toast.LENGTH_SHORT).show()
+        }
     }
 }
